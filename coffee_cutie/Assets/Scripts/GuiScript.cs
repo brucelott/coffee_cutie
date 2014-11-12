674cecb8a643542031c19c5a0b60a0470e34be1b
Bruce Lott <bruce.lott@gmail.com>
2014-11-11 22:35:50 -0800

    Added gui script w/ demo scene

---
+++ coffee_cutie/Assets/Scripts/GuiScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System;
+
+public class GuiScript : MonoBehaviour {
+
+	public CustomGuiButton milkSoy;
+	public CustomGuiButton milkWhole;
+	public CustomGuiButton milkTwoPercent;
+	
+	void Start(){
+		milkSoy.init();
+		milkWhole.init();
+		milkTwoPercent.init();
+	}
+	
+	void OnGUI() {
+		if(milkSoy.OnMouseClick()){ 
+			Debug.Log("clicked milkSoy");
+		}
+		else if(milkWhole.OnMouseClick()){ 
+			Debug.Log("clicked milkWhole");
+		}
+		else if(milkTwoPercent.OnMouseClick()){ 
+			Debug.Log("clicked milkTwoPercent");
+		}
+	}
+}
+
+[Serializable] // This exposes public variables to Unity's Inspector.
+public class CustomGuiButton{
+	public GUIStyle style = new GUIStyle();
+	public Texture2D image;
+	public int X = 100;
+	public int Y = 100;
+	
+	public void init(){
+		style.normal.background = image;
+	}
+	
+	public bool OnMouseClick(){
+		return GUI.Button(new Rect(X,Y, image.width,image.height), "", style);
+	}
+}		
