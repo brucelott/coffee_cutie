c7ec828a6a0b06b661c251aac90d462aafb2711e
brulo <bruce.lott@gmail.com>
2015-01-17 18:47:20 -0800

    Print one set of ingredients.
    Implemented a static page of the recipebook reading from RecipeBook
    class.
    Moved all related script files to Scripts/RecipeBook/

---
+++ Assets/Scripts/RecipeBook/RecipeBookGUI.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RecipeBookGUI : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+}
---
+++ Assets/Scripts/RecipeBook/RecipeBookText.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class RecipeBookText : MonoBehaviour {
+
+	public Text text;
+	RecipeBook recipeBook = new RecipeBook();
+
+	void Start () 
+	{
+		text = gameObject.GetComponent<Text>();
+		/* text.text = "Hot Vanilla Latte:\n\n- Ingredient 1\n- Ingredient 2\n- Ingredient 3"; */
+		/* text.text = recipeBook.translate(recipeBook.allDrinksArray[0].DrinkIngredients[0]); */
+		text.text = getRecipe(recipeBook.allDrinksArray[0]);
+	}
+
+	private string getRecipe(Drink drink) 
+	{
+		string output = "";
+		output += drink.name.ToUpper();
+		output += "\n";
+		for(int i = 0; i < drink.DrinkIngredients.Length; i++)
+		{
+			output += recipeBook.translate(drink.DrinkIngredients[i]);
+			output += "\n";
+		}
+
+		return output;
+	}
+
+}
